ebacd0674f3f6ae2f57c31940519c17134de83a7
Samuel Nunes <samuelsalomao883@gmail.com>
2025-10-22 21:29:20 -0300

    Criacao da rota de dados pessoais

--- src/controllers/personalDataController.js
+++ src/controllers/personalDataController.js
@@ -10,5 +10,47 @@
 */
 
 export const getPersonalData = async (req, res) => {
+
+  try {
+    const { gender } = req.query;
+
+    if (gender && !['m', 'f'].includes(gender.toLowerCase())) {
+      return res.status(400).json({ 
+        error: "Gênero inválido! Use 'M' ou 'F'." 
+      });
+    }
+    
+    const genderFormatted = setGender(req.query.gender);
+    
+    
+
+    const data = {
+      name: null,
+      motherName: null,
+      fatherName: null,
+      CPF: null,
+      RG: null,
+      dateOfBirth: null,
+      gender: genderFormatted,
+      maritalStatus: null,
+      nationality: null
+    }
   
+    res.status(201).json({ result: data });
+
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+}
+
+function setGender(gender) {
+  const g = gender.toLowerCase().trim();
+
+  if (g === "m") {
+    return "masculino";
+  } else if (g === "f") {
+    return "feminino";
+  }
+
+  throw new Error("Sexo invalido! Selecione 'M' ou 'F'.");
 }
--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,19 @@
 import { Router } from "express";
+import { getPersonalData } from "./controllers/personalDataController.js";
 
 const routes = Router();
 
-routes.get('/personal-data');
-routes.get('/documents');
-routes.get('/address');
-routes.get('/contact');
-routes.get('/financial-data');
-routes.get('/vehicle-data');
-routes.get('/professional-data');
-routes.get('/additional-contextual-data');
+routes.get('/', (req, res) => {
+  res.status(201).json({ message: "Seja bem vindo a API Brazilian Mock" });
+});
+
+routes.get('/personal-data', getPersonalData);
+// routes.get('/documents');
+// routes.get('/address');
+// routes.get('/contact');
+// routes.get('/financial-data');
+// routes.get('/vehicle-data');
+// routes.get('/professional-data');
+// routes.get('/additional-contextual-data');
 
 export default routes;
