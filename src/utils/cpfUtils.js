7b1d3ef82e6f6ade944e82c097cdc3ff5af0cbc9
Samuel Nunes <samuelsalomao883@gmail.com>
2025-10-29 08:33:23 -0300

    Atualizacao no sistema de geracao de CPF

--- src/utils/cpfUtils.js
+++ src/utils/cpfUtils.js
@@ -1,26 +1,54 @@
 export function generateCPF() {
   const cpfArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
   // for (let i = 0; i < 9; i++) {
   //   cpfArray.push(Math.floor(Math.random() * 10));
   // }
 
-  let count = 10;
+  const cpf = getCheckDigit(cpfArray);
+
+  return cpf;
+}
+
+function getCheckDigit(cpfArray) {
+  if (cpfArray.length < 9 || cpfArray.length > 10) {
+    throw new Error('CPF fora do tamanho ideal!');
+  }
+
+  let count = 0;
+
+  if (cpfArray.length === 9) {
+    count = 10;
+  } else if (cpfArray.length === 10) {
+    count = 11;
+  }
+
+  const cpfSumArray = [];
 
   cpfArray.forEach((number, index) => {
-    cpfArray[index] = number * count;
+    cpfSumArray[index] = number * count;
 
     count--;
   })
 
-  const CPFSum = cpfArray.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+  const CPFSum = cpfSumArray.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
   const firstCheckDigit = CPFSum % 11;
 
   if (firstCheckDigit < 2) {
-    cpfArray.push(0)
+    cpfArray.push(0);
   } else {
     cpfArray.push(11 - firstCheckDigit);
   }
 
-  console.log("cpfArray =>", cpfArray);
+  if (cpfArray.length === 11) {
+    formatCPF(cpfArray);
+
+    return cpfArray;
+  }
+
+  cpfArray = getCheckDigit(cpfArray);
+  return cpfArray;
+}
+
+function formatCPF(cpf) {
+  
 }
