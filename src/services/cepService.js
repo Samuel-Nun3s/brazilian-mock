c871798ca06935ff7b8c643a60a2e23d2443d9d9
Samuel Nunes <samuelsalomao883@gmail.com>
2025-11-03 08:31:18 -0300

    Criacao do service de geracao de CEP

--- src/services/cepService.js
+++ src/services/cepService.js
@@ -1,5 +1,6 @@
-import { generateCEP } from "../utils/cepGenerator";
-import { loadJSON } from "../utils/jsonLoader";
+import { generateCEP, removeCEPMask } from "../utils/cepGenerator.js";
+import { loadJSON } from "../utils/jsonLoader.js";
+import { APIURLS } from "../config/api.js";
 
 function getAddressesInCache() {
   const data = loadJSON('addressesCache.json');
@@ -10,9 +11,28 @@ function isCEPInCache(cep) {
   const addressesInCache = getAddressesInCache();
 
   if (addressesInCache[cep]) {
-    return true;
+    return addressesInCache[cep];
   }
 
   return false;
 }
 
+function getAddress(CEP) {
+  const CEPFormatted = removeCEPMask(CEP);
+
+  const CEPExistsInCache = isCEPInCache(CEPFormatted);
+
+  if (CEPExistsInCache) {
+    return CEPExistsInCache;
+  }
+
+  return fetchCEPFromVIACEP(CEPFormatted);
+}
+
+async function fetchCEPFromVIACEP(cep) {
+  const response = await fetch(APIURLS.VIACEP, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json"
+  }})
+}
